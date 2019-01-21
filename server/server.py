3b4ded23afdf26e91f097bdee4a2da10f102a9cf
rheaton64 <rheaton@10.144.5.44>
2019-01-20 23:14:25 -0500

    worked on server w mongo

--- server/server.py
+++ server/server.py
@@ -1,10 +1,14 @@
 import os
 
 from flask import Flask
+from flask import render_template
+from flask_pymongo import PyMongo
 
 os.environ['FLASK_DEBUG'] = "1" # DO NOT USE IN PRODUCTION
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dev_data"
+mongo = PyMongo(app)
 
 @app.route("/hello/")
 def hello():
@@ -13,6 +17,12 @@ def hello():
 @app.route("/test/")
 def test():
     return "test"
+
+@app.route("/data/")
+def home_page():
+    online_users = mongo.db.data
+    return render_template("user.html",
+        online_users=online_users)
 
 if __name__ == '__main__':
     app.run()
