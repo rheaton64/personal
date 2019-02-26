7f9fe696268bbe26b421263b57fd4d1e6bcba010
Ryan <ryguyheaton@gmail.com>
2019-02-26 12:49:33 -0500

    worked on csv stuff

--- server/csvtest.py
+++ server/csvtest.py
@@ -7,16 +7,18 @@ import csv
 
 class Assignment:
     
-    def __init__(self, className, assType, assName, assDesc, dateAss, weekdayDue):
+    def __init__(self, className, assType, assName,dateAss, weekdayDue):
         self.className = className
         self.assType = assType
         self.assName = assName
-        self.assDesc = assDesc
         self.dateAss = dateAss
         self.weekdayDue = weekdayDue
 
     def __str__(self):
-        return "Class: " + self.className + " , Type: " + self.assType + " , Name: " + self.assName + " , Desc: " + self.assDesc + " , Assign Date: " + self.dateAss + " , Due Date: " + str(self.weekdayDue)
+        return "Class: " + self.className + " , Type: " + self.assType + " , Name: " + self.assName + " , Assign Date: " + self.dateAss + " , Due Date: " + str(self.weekdayDue)
+    
+    def toString(self):
+        return self.className + ',,'+ self.assType + ',,' + self.assName + ',,' + self.dateAss + ',,' + str(self.weekdayDue)
 
 # literally just declaring a bunch of variables fo use in the future
 # if you enjoy the names, thank you, I did too, "assignments" have a conveniently fun abbreviation
@@ -32,9 +34,22 @@ friAss = []
 satAss = []
 assForDay = [sunAss, monAss, tueAss, wedAss, thuAss, friAss, satAss]
 
+def parseAssignments(startIndex, endIndex, arr, day):
+    lenSect = endIndex - startIndex
+    if lenSect < 5:
+        assign = Assignment(arr[startIndex], "ERROR", "INVALID_DATA", "", day)
+    else:
+        if lenSect > 5:
+            arr.pop(endIndex - 4)
+            endIndex -= 1
+        assign = Assignment(arr[startIndex], arr[startIndex+1], arr[startIndex+2], arr[endIndex-1], day)
+    assForDay[day].append(assign.toString())
+    if endIndex+1 != len(arr):
+        parseAssignments(endIndex+1, arr.index('', endIndex+4), arr, day)
+
 # now this is where the magic happens
 # if you don't understand how this works, just google it, using the python "csv" library
-with open('/Users/rheaton/Documents/code/personal/server/ryan_ass.csv') as csv_file:
+with open('/Users/rheaton/Documents/code/personal/server/spencer_ass.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -54,23 +69,13 @@ with open('/Users/rheaton/Documents/code/personal/server/ryan_ass.csv') as csv_f
             day = 0
             for index in row:
                 arr = index.split("\n")
-                indexCounter = 0
-                for i in range(len(arr)/6):
-                    tempClass = arr[indexCounter]
-                    indexCounter += 1
-                    tempType = arr[indexCounter]
-                    indexCounter += 1
-                    tempName = arr[indexCounter]
-                    indexCounter += 2
-                    tempDate = arr[indexCounter]
-                    indexCounter += 2
-                    assign = Assignment(tempClass, tempType, tempName, "", tempDate, day)
-                    assForDay[day].append(assign)
-                    numAss += 1
+                if len(arr) > 1:
+                    parseAssignments(0, arr.index('', 4), arr, day)
                 day += 1
         #else:
             #print(row)
         line_count += 1
+
 
 # print(name)
 # print(week)
@@ -106,6 +111,7 @@ post = {
     "student_name": name,
     "date": week,
     "number_of_assignments": numAss,
+    "assignmnets": assForDay
 }
 
 print(post)
